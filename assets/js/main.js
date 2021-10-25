8f3a635974e147362b702c6e6506c358f4a90477
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-25 16:59:19 +0200

    fatto ciclo post, funziona

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@
 let postList = [
     {
         "id": 0,
-        "contenuto": "lorem12",
+        "contenuto": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis suscipit repellendus vitae!",
         "immagine": "https://i.picsum.photos/id/726/450/230.jpg?hmac=oUuekS6Ztvm9rPNulsQJX__Mv6o0MWsSJlgc7p_Ias0",
         "autore": ["Zack Canasta", "https://i.picsum.photos/id/518/50/50.jpg?hmac=NSJ8qtVF-xr09AM3P7psapk3GJSe_8ZakwMlrUInNm0"],
         "likesN": 0,
@@ -22,8 +22,8 @@ let postList = [
 
     {
         "id": 0,
-        "contenuto": "lorem16",
-        "immagine": "https://i.picsum.photos/id/571/450/230.jpg?hmac=ks_cxdfFf-7kYe5tKp44iaEqbpVk-8RBt2UdNm8ph7k0",
+        "contenuto": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident nam unde itaque reprehenderit dolorum ex perferendis.6",
+        "immagine": "https://i.picsum.photos/id/91/450/230.jpg?hmac=3-Dod75DvwRAgITysdnHiuxBQvgjOm1FdvsfF5gLcC8",
         "autore": ["Bruno Liegi-Bastogne-Liegi", "https://i.picsum.photos/id/274/50/50.jpg?hmac=wd1V_89I-BBmuYYXuHgJujEmwilyBN6LcQ8EkR--umM"],
         "likesN": 0,
         "date": "23/04/2021"
@@ -32,7 +32,7 @@ let postList = [
 
     {
         "id": 0,
-        "contenuto": "lorem9",
+        "contenuto": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad.",
         "immagine": "https://i.picsum.photos/id/1021/450/230.jpg?hmac=CubCGyWGZFi6NQ2DkgSOa8RtYOKy2XXbLIoC59fPjcM",
         "autore": ["Riccardino Fuffolo", ""],
         "likesN": 0,
@@ -42,7 +42,7 @@ let postList = [
 
     {
         "id": 0,
-        "contenuto": "lorem19",
+        "contenuto": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae dignissimos voluptate magnam ipsa accusantium deserunt autem fugit ad vero.",
         "immagine": "https://i.picsum.photos/id/664/450/230.jpg?hmac=EUFJyOUOG81Z1E8u2Aj6Qw3IqB8Y6nhF21fhbwQjazE",
         "autore": ["Amalia Frellioje", "https://i.picsum.photos/id/30/50/50.jpg?hmac=NwG4W4vHtEenBxNv1J4l62VoTpxnoLu8PXAGf1bdGa0"],
         "likesN": 0,
@@ -58,42 +58,79 @@ console.log(postList[1].autore[1]);
 //ora faccio ciclo for per stampare i post, prendendolo chiaramente da quello che ho fatto in HTML
 //metto l'HTML in una const per lasciare il ciclo un po' più pulito
 
-let post = `<div class="head d-flex">
-<div class="pro_pic col_2 text-center">
-    <img src="${postList[0].autore[1]}" alt="">
-</div>
 
-<div class="user_name px-3 py-2 col-10">
-    <p class="name my-0">${postList[0].autore[0]}</p>
-    <p class="date my-0">${postList[0].date}</p>
-</div>
+//adatto HTML con le proprietà dell'array, prima per il primo post, se funziona lo metto nel ciclo (al momento tralascio contatore, bonus, event listener per il like etc)
 
-</div>
-<p class= "post_text mt-3">
-${postList[0].contenuto}
-</p>
+// niente, devo mettere il post dentro al ciclo sennò ho problemi a definire i
+// let postHTML = `
+// <div class="post container bg-white p-3 my-2">
+// <div class="head d-flex">
+// <div class="pro_pic col_2 text-center">
+//     <img src="${postList[i].autore[1]}" alt="">
+// </div>
 
-<div class="post_pic text-center">
-<img src="${postList[0].immagine}">
-</div>
+// <div class="user_name px-3 py-2 col-10">
+//     <p class="name my-0">${postList[i].autore[0]}</p>
+//     <p class="date my-0">${postList[i].date}</p>
+// </div>
 
-<div class="likes d-flex py-3">
-<div class="like col-6 text-center">
-    <i class="fas fa-thumbs-up"></i> Mi piace
-</div>
-<div class= "like_numb col-6 text-center">
-    Piace a <span class="bold">80</span> persone
-</div>
-</div>`;
+// </div>
+// <p class= "post_text mt-3">
+// ${postList[i].contenuto}
+// </p>
 
-console.log(post);
+// <div class="post_pic text-center">
+// <img src="${postList[i].immagine}">
+// </div>
 
-document.querySelector(".post.container").innerHTML = post;
+// <div class="likes d-flex py-3">
+// <div class="like col-6 text-center">
+//     <i class="fas fa-thumbs-up"></i> Mi piace
+// </div>
+// <div class= "like_numb col-6 text-center">
+//     Piace a <span class="bold">80</span> persone
+// </div>
+// </div>
+// </div>`;
+
+// console.log(post);
+
+// document.getElementById("post_list").innerHTML = post;
 
 
 
+//questo è il ciclo for semplice semplice, dovrebbe fare tutto quanto fatto per il singolo post ma per tutti
+for (let i = 0; i < postList.length; i++) {
+    let post= `
+        <div class="post container bg-white p-3 my-2">
+        <div class="head d-flex">
+        <div class="pro_pic col_2 text-center">
+            <img src="${postList[i].autore[1]}" alt="">
+        </div>
 
-// for (let i = 0; i < postList.length; i++) {
-//     const post = postList[i];
-//     document.querySelector(".post.container").innerHTML += post;
-// }
+        <div class="user_name px-3 py-2 col-10">
+            <p class="name my-0">${postList[i].autore[0]}</p>
+            <p class="date my-0">${postList[i].date}</p>
+        </div>
+
+        </div>
+        <p class= "post_text mt-3">
+        ${postList[i].contenuto}
+        </p>
+
+        <div class="post_pic text-center">
+        <img src="${postList[i].immagine}">
+        </div>
+
+        <div class="likes d-flex py-3">
+        <div class="like col-6 text-center">
+            <i class="fas fa-thumbs-up"></i> Mi piace
+        </div>
+        <div class= "like_numb col-6 text-center">
+            Piace a <span class="bold">80</span> persone
+        </div>
+        </div>
+        </div>`;
+
+        document.getElementById("post_list").innerHTML += " " + post;
+}
