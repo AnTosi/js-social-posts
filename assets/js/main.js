973974e61880a36a8db10d152b3cea80e70f92a6
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-25 16:05:47 +0200

    creato l'array

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+//e ora viene il bello
+//devo stampare una lista di post partendo da un array di oggetti
+
+//attributi:
+//id
+//contenuto
+//immagine
+//autore (nome ed avatar)
+//data creazione
+
+//secondo array solo id dei post a cui abbiamo dato like
+
+let postList = [
+    {
+        "id": 0,
+        "contenuto": "lorem12",
+        "autore": ["Zack Canasta", "https://i.picsum.photos/id/518/50/50.jpg?hmac=NSJ8qtVF-xr09AM3P7psapk3GJSe_8ZakwMlrUInNm0"],
+        "likesN": 0,
+        "date": ""
+
+    },
+
+    {
+        "id": 0,
+        "contenuto": "lorem16",
+        "autore": ["Riccardino Fuffolo", "https://i.picsum.photos/id/518/50/50.jpg?hmac=NSJ8qtVF-xr09AM3P7psapk3GJSe_8ZakwMlrUInNm0"],
+        "likesN": 0,
+        "date": ""
+
+    },
+
+    {
+        "id": 0,
+        "contenuto": "lorem9",
+        "autore": ["Bruno Liegi-Bastogne-Liegi", "https://i.picsum.photos/id/518/50/50.jpg?hmac=NSJ8qtVF-xr09AM3P7psapk3GJSe_8ZakwMlrUInNm0"],
+        "likesN": 0,
+        "date": ""
+
+    },
+
+    {
+        "id": 0,
+        "contenuto": "lorem19",
+        "autore": ["Amalia Frellioje", "https://i.picsum.photos/id/518/50/50.jpg?hmac=NSJ8qtVF-xr09AM3P7psapk3GJSe_8ZakwMlrUInNm0"],
+        "likesN": 0,
+        "date": ""
+
+    },
+]
+
+//creato l'array con ciò che chiedeva
